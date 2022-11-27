4860e675f7a3477fa1b44e618ebc031ae3304b17
Burning Glass <carlgmanw@gmail.com>
2022-11-27 21:09:38 +0000

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,27 @@
+# import flask
+from flask import Flask, render_template, request
+
+# initilize flask
+app = Flask(__name__)
+
+
+# setup the route
+@app.route('/hello')
+def hello():
+    return "Hello, World!"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        value_one = int(request.form.get('number-one'))
+        value_two = int(request.form.get('number-two'))
+        total = value_one + value_two
+        return render_template('index.html', value=total)
+    return render_template('index.html')
+
+# run the server
+if __name__ == '__main__':
+    app.run()
+
+# boom!
